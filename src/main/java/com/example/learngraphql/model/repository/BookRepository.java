1b5aad46769a81685d7f013cf3e5f09c1ba01f22
Manjeet Chhikara <mchhikara@Manjeets-MacBook-Pro.local>
2024-02-12 21:05:00 -0500

    changing test data

--- src/main/java/com/example/learngraphql/model/repository/BookRepository.java
+++ src/main/java/com/example/learngraphql/model/repository/BookRepository.java
@@ -31,7 +31,7 @@ public class BookRepository {
         books = Arrays.asList(
                 new Book(1, "Science 1", 500, Rating.FIVE_STAR, authorRepository.findByFullName("Abhimanyu Chhikara")),
                 new Book(2, "Science 2", 650, Rating.THREE_STAR, authorRepository.findByFullName("Test Writer")),
-                new Book(3, "Science 3", 350, Rating.ONE_STAR, authorRepository.findByFullName("Manjeet Chhikara")),
+                new Book(3, "Science 3", 350, Rating.TWO_STAR, authorRepository.findByFullName("Manjeet Chhikara")),
                 new Book(4, "Science 4", 880, Rating.FOUR_STAR, authorRepository.findByFullName("Abhimanyu Chhikara")),
                 new Book(5, "Science 5", 675, Rating.TWO_STAR, authorRepository.findByFullName("Manjeet Chhikara"))
         );
